58bac28ba24b0814f51af736b946637f24680472
ssaladd <58093617+ssaladd@users.noreply.github.com>
2021-03-08 00:34:09 -0500

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,5 +1,6 @@
 import random
 import names
+import json
 from random_username.generate import generate_username
 
 hi = [
@@ -11,7 +12,6 @@ hi = [
     "outlook.com",
     "mail.com",
 ]
-
 me = [
     "facebook.com",
     "mac.com",
@@ -21,7 +21,6 @@ me = [
     "yahoo.co.uk",
     "yahoo.com.br",
 ]
-
 lo = [
     "googlemail.com",
     "yahoo.co.in",
@@ -32,27 +31,23 @@ lo = [
     "protonmail.com",
 ]
 
-class Email:
-    username = ""
-    domain = ""
-
+class Account:
     def __init__(self):
-        self.username = self.random_username()
-        self.domain = self.random_domain()
-
-    def display(self):
-        return ""+self.username+"@"+self.domain
+        self.sex       = random.choices(['male', 'female'], weights=[.46, .54])[0]
+        self.firstname = names.get_first_name(self.sex)
+        self.lastname  = names.get_last_name()
+        self.username  = self.random_username()
+        self.domain    = self.random_domain()
+        self.email     = self.username+'@'+self.domain
 
     def random_username(self):
         num = random.choices(
             [str(random.randint(0, 10)), str(random.randint(11, 99)), str(random.randint(99, 999))],
             weights=[.58, .26, .14]
         )[0]
-        sex = random.choices(['male', 'female'], weights=[.45, .55])[0]
-        name = names.get_last_name() if random.randint(0, 1) == 0 else names.get_first_name(sex)
-        word = generate_username(1)[0][:-1] if random.randint(0, 1) == 0 else generate_username(1)[0][:-1]+num
 
-        #mess with the numbes here to change up the usernames
+        name = self.lastname if random.randint(0, 1) == 0 else self.firstname
+        word = generate_username(1)[0][:-1] if random.randint(0, 1) == 0 else generate_username(1)[0][:-1]+num
         first_section = random.randint(2, int(len(word) / 2))
         second_section = random.randint(int(len(word) / 4), len(word) - 3)
 
@@ -64,11 +59,36 @@ class Email:
         return selection
 
 def main():
+    accounts = {}
     iterations = int(input("iterations >> "))
-    f = open("output.txt", "r+")
     for i in range(iterations):
-        email = Email()
-        f.write(email.display() + " \n")
+        acc = Account()
+        print('='*75)
+        print("firstname: ", acc.firstname)
+        print("lastname: ", acc.lastname)
+        print("sex: ", acc.sex)
+        print("username: ", acc.username)
+        print("domain: ", acc.domain)
+        print("email: ", acc.email)
+        print('-'*75)
+        check = str(input("y or n >> "))
+        print('='*75)
+        if (check == 'y'): 
+            accounts.update(
+                {
+                    acc.username : {
+                        "username":acc.username,
+                        "email":acc.email,
+                        "name":acc.firstname+' '+acc.lastname,
+                        "password": "botpass7",
+                    }
+                }
+            )
+            
+
+    print(accounts)
+    f = open("output.json", "w")
+    json.dump(accounts, f, indent=2)
     f.close()
 
 if __name__ == "__main__":
